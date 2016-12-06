2d700688eebb5be230781bd6704dc6387d3bd967
jah4ck <johann.simon.web@gmail.com>
2016-12-06 14:33:55 +0100

    gestion des logs

--- DownloadFile/Program.cs
+++ DownloadFile/Program.cs
@@ -14,26 +14,24 @@ namespace DownloadFile
         static void Main(string[] args)
         {
             ReferenceWSCtrlPc.WSCtrlPc ws = new ReferenceWSCtrlPc.WSCtrlPc();
+            Trace MyTrace = new Trace();
             string codeappli = "DownloadFile.exe";
             DateTime datetraitement = DateTime.Now;
             string linkDownload = ConfigurationManager.AppSettings["linkDownload"];
             Object Guid = Registry.GetValue(@"HKEY_USERS\.DEFAULT\Software\CtrlPc\Version", "GUID", null);
             try
             {
-                
-                ws.TraceLog(Guid.ToString(), datetraitement, codeappli, 2, "Téléchargement de " + args[1]);
+                MyTrace.WriteLog("Téléchargement de " + args[1],2,codeappli);
                 string uri = linkDownload + args[1];
                 string dest = @"C:\ProgramData\CtrlPc\"+args[0]+@"\"+args[1];
                 WebClient webClient = new WebClient();
                 webClient.DownloadFile(new Uri(uri), dest);
                 datetraitement = DateTime.Now;
-                ws.TraceLog(Guid.ToString(), datetraitement, codeappli, 2, "Fin du téléchargement de " + args[1]);
+                MyTrace.WriteLog("Fin du téléchargement de " + args[1], 2, codeappli);
             }
             catch (Exception err)
             {
-                Console.WriteLine(err.Message);
-                datetraitement = DateTime.Now;
-                ws.TraceLog(Guid.ToString(), datetraitement, codeappli, 1,"Erreur lors du téléchargement de "+args[1]+" --> "+ err.Message);
+                MyTrace.WriteLog("Erreur lors du téléchargement de " + args[1] + " --> " + err.Message, 1, codeappli);
             }
             
         }
